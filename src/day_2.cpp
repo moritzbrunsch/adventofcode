f26041a25efc223afb499ed1dd2c7cbe5659b471
Moritz Brunsch <moritz_brunsch@protonmail.com>
2025-06-04 16:38:01 +0200

    finished day two part one
    added logic to check validity of reports \n fixed default file name

--- src/day_2.cpp
+++ src/day_2.cpp
@@ -1,11 +1,52 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<sstream>
 #include<vector>
 
+int abs_dist(int num_1, int num_2){
+    return num_1 < num_2 ? num_2 - num_1 : num_1 - num_2;
+}
+
+
 int check_validity(std::string report){
-    // TODO add logic
-    return true;
+    // parse string
+    std::stringstream line_stream(report);
+    std::string number_str;
+    std::vector<int> report_nums;
+    while(line_stream >> number_str){
+        report_nums.push_back(std::stoi(number_str));
+    }
+
+    //check report safety
+    int last_num = -1;
+    bool asc_set = false;
+    bool asc;
+    for(int num: report_nums){
+        if(last_num == -1){
+            last_num = num;
+        } else{
+            // ascencion criterion
+            if(asc_set == true){
+                if((asc && !(last_num < num)) || (!asc && (last_num < num))){
+                    std::cout << "asc " << last_num << " " << num << " " << asc << std::endl;
+                    return 0;
+                }
+            } else{
+                asc = last_num < num ? true : false;
+                asc_set = true;
+            }
+            // jump distance criterion
+            int dist = abs_dist(last_num, num);
+            if(!(dist < 4 && dist > 0)){
+                std::cout << "dist " << last_num << " " << num << std::endl;
+                return 0;
+            }
+            last_num = num;
+        }
+    }
+    std::cout << "done " << last_num << std::endl;
+    return 1;
 }
 
 int main(int argc, char **argv){
@@ -34,7 +75,7 @@ int main(int argc, char **argv){
     }
     input.close();
 
-    std::cout << valid_reports << std::endl;
+    std::cout << "Valid reports: " << valid_reports << std::endl;
 
     return 0;
 }
